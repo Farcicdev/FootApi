b997cb52fe780430b46bc4a16ffdbb4c4157924e
farcicDev <augustofarcic@gmail.com>
2026-05-12 10:16:01 -0300

    testando o push pelo terminal que tava dando problema aqui

--- src/main/java/farcic/dev/footApi/dto/request/ClubRequestDto.java
+++ src/main/java/farcic/dev/footApi/dto/request/ClubRequestDto.java
@@ -2,9 +2,11 @@ package farcic.dev.footApi.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 
 import java.time.LocalDate;
 
+@Builder
 public record ClubRequestDto(
         @NotBlank
         String name,
--- src/test/java/farcic/dev/footApi/service/ClubServiceTest.java
+++ src/test/java/farcic/dev/footApi/service/ClubServiceTest.java
@@ -1,21 +1,69 @@
 package farcic.dev.footApi.service;
 
+import farcic.dev.footApi.dto.request.ClubRequestDto;
+import farcic.dev.footApi.dto.response.ClubDetatilsResponse;
+import farcic.dev.footApi.dto.response.StadiumResponseDto;
+import farcic.dev.footApi.entity.Club;
+import farcic.dev.footApi.entity.Stadium;
+import farcic.dev.footApi.mapper.ClubMapper;
+import farcic.dev.footApi.repository.ClubRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDate;
 
 class ClubServiceTest {
 
+    @InjectMocks
+    ClubService service;
+
+    @Mock
+    ClubRepository clubRepository;
+    @Mock
+    ClubMapper mapper;
+    @Mock
+    StadiumService stadiumService;
+
+    @Test
+    void save() {
+        //Arrenge
+        ClubRequestDto request = ClubRequestDto.builder()
+                .name("Teste")
+                .founded(LocalDate.of(2020, 1, 1))
+                .urlImg("http://teste.com/img.png")
+                .stadiumId(1L)
+                .build();
+        Club teste = Club.builder()
+                .id(1L)
+                .name("Teste")
+                .founded(LocalDate.of(2020, 1, 1))
+                .urlImg("http://teste.com/img.png")
+                .stadium(Stadium.builder().id(10L).build())
+                .build();
+
+        Stadium stadium = Stadium.builder()
+                .id(10L)
+                .name("Teste Stadium")
+                .city("Teste City")
+                .capacity(50000)
+                .build();
+
+        Mockito.when(mapper.toEntity(request)).thenReturn(teste);
+        Mockito.when(stadiumService.findById(stadium.getId())).thenReturn(stadium);
+        //Action
+        StadiumResponseDto execute = stadiumService.createStadium(request);
+
+    }
+
     @Test
     void findAll() {
+
     }
 
     @Test
     void findById() {
-    }
-
-    @Test
-    void save() {
     }
 
     @Test
